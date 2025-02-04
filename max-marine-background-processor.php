a5660a66ef6652e378135534eabfeeb4053db6b2
David Baumwald <david.baumwald@gmail.com>
2025-02-04 10:50:04 -0500

    Initial commit

--- max-marine-background-processor.php
+++ max-marine-background-processor.php
@@ -22,7 +22,7 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       max-marine-background-processor
  * Domain Path:       /languages
- * GitHub Plugin URI: https://github.com/GH_REPO_URL
+ * GitHub Plugin URI: https://github.com/dream-encode/max-marine-background-processor
  * Primary Branch:    main
  * Release Asset:     true
  */
